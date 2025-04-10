8f08b963b4811bcbc7ac545ed39863bf47b4c56e
eN <seizuresthree@gmail.com>
2025-04-10 12:34:16 -0700

    Added two more object pools

--- VGP336/28_HelloMemory/WinMain.cpp
+++ VGP336/28_HelloMemory/WinMain.cpp
@@ -34,20 +34,77 @@ private:
     std::string mName;
 };
 
+class Cow
+{
+public:
+    Cow()
+    {
+        mId = ++sUniqueId;
+        mName = "Cow";
+    }
+
+    void Log()
+    {
+        LOG("Name: %s | Id: %d", mName.c_str(), mId);
+    }
+
+private:
+    int mId = 0;
+    std::string mName;
+};
+
+class Hay
+{
+public:
+    Hay()
+    {
+        mId = ++sUniqueId;
+        mName = "Hay";
+    }
+
+    void Log()
+    {
+        LOG("Name: %s | Id: %d", mName.c_str(), mId);
+    }
+
+private:
+    int mId = 0;
+    std::string mName;
+};
+
 int WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
     TypedAllocator studentPool = TypedAllocator<Student>("StudentPool", 100);
+    TypedAllocator cowPool = TypedAllocator<Cow>("CowPool", 8);
+    TypedAllocator hayPool = TypedAllocator<Hay>("HayPool", 50);
 
     std::vector<Student*> students;
-    //test New
+    std::vector<Cow*> cows;
+    std::vector<Hay*> hays;
+
+    //--- test New ----------------------------------
     for (uint32_t i = 0; i < 100; ++i)
     {
         Student* newStudent = studentPool.New();
         newStudent->Log();
         students.push_back(newStudent);
+
+        if (i < 50)
+        {
+            Hay* newHay = hayPool.New();
+            newHay->Log();
+            hays.push_back(newHay);
+        }
+
+        if (i < 8)
+        {
+            Cow* newCow = cowPool.New();
+            newCow->Log();
+            cows.push_back(newCow);
+        }
     }
 
-    //test Deletion
+    //--- test Deletion ----------------------------------
     for (uint32_t i = 0; i < 60;++i)
     {
         Student* student = students.back();
@@ -55,7 +112,18 @@ int WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
         students.pop_back();
     }
 
-    //test Addition
+    for (uint32_t i = 0; i < 5;++i)
+    {
+        Cow* cow = cows.back();
+        cowPool.Delete(cow);
+        cows.pop_back();
+
+        Hay* hay = hays.back();
+        hayPool.Delete(hay);
+        hays.pop_back();
+    }
+
+    //--- test Addition ----------------------------------
     for (uint32_t i = 0;i < 55;++i)
     {
         Student* student = studentPool.New("Lalalalala");
@@ -63,12 +131,35 @@ int WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
         students.push_back(student);
     }
 
-    //clear all
+    for (uint32_t i = 0;i < 2;++i)
+    {
+        Cow* cow = cowPool.New();
+        cow->Log();
+        cows.push_back(cow);
+
+        Hay* hay = hayPool.New();
+        hay->Log();
+        hays.push_back(hay);
+    }
+
+    //--- clear all ----------------------------------
     for (Student* student : students)
     {
         studentPool.Delete(student);
     }
     students.clear();
+
+    for (Cow* cow : cows)
+    {
+        cowPool.Delete(cow);
+    }
+    cows.clear();
+
+    for (Hay* hay : hays)
+    {
+        hayPool.Delete(hay);
+    }
+    hays.clear();
 
     return (0);
 }
